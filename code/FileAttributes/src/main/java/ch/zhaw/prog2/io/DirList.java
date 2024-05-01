d69cf0c178992d351bf4629dc1a9c7cacce3ca98
Tobïas Kugel <kugelto@outlook.com>
2024-05-01 17:06:54 +0200

    implement DirList

--- code/FileAttributes/src/main/java/ch/zhaw/prog2/io/DirList.java
+++ code/FileAttributes/src/main/java/ch/zhaw/prog2/io/DirList.java
@@ -16,8 +16,21 @@ public class DirList {
     public static void main(String[] args) {
         String pathName = args.length >= 1 ? args[0] : ".";
         File file = new File(pathName);
-        // todo: print metadata for file or contained files if it is a directory
+        if (!file.exists()) {
+            System.err.println("File or directory does not exist: " + pathName);
+            System.exit(1);
+        }
 
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    System.out.println(printFileMetadata(f));
+                }
+            }
+        } else {
+            System.out.println(printFileMetadata(file));
+        }
     }
 
     /** Write metadata of given file on a line with the following format:<br>
@@ -34,6 +47,18 @@ public class DirList {
      * @return String formatted as described above.
      */
     public static String printFileMetadata(File file) {
-        return file.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append(file.isDirectory() ? 'd' : 'f');
+        sb.append(file.canRead() ? 'r' : '-');
+        sb.append(file.canWrite() ? 'w' : '-');
+        sb.append(file.canExecute() ? 'x' : '-');
+        sb.append(file.isHidden() ? 'h' : '-');
+        sb.append(" ");
+        sb.append(dateFormat.format(file.lastModified()));
+        sb.append(" ");
+        sb.append(String.format("%10d", file.length()));
+        sb.append(" ");
+        sb.append(file.getName());
+        return sb.toString();
     }
 }
